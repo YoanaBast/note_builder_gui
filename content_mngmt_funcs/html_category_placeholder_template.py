b2c516ada71582728993eac46b588da3e2efbdfc
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-09-24 02:50:27 +0300

    fixed resize with viewport and css

--- content_mngmt_funcs/html_category_placeholder_template.py
+++ content_mngmt_funcs/html_category_placeholder_template.py
@@ -5,13 +5,12 @@ category_template = textwrap.dedent("""\
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=0.6, maximum-scale=3.0">
 <title>{CATEGORY_NAME} topics</title>
 <link rel="stylesheet" href="style.css">
 </head>
     <body>
     <script src="scripts/modal.js"></script>
-    <script src="scripts/responsive.js"></script>
         <h1>Programming sticky notes! &#x1F49C;</h1>
         <header>
             <nav>
